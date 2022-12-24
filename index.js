3210aabbbcac8748f4102d81f2614cf20ecfb552
Matheus <matheus.gregorin@sptech.school>
2022-12-23 21:21:33 -0300

    new project not databases

---
+++ index.js
@@ -0,0 +1,38 @@
+
+ // -------------------REQUISIÇÔES--------------------------------------
+ 
+ require('dotenv/config');
+ var express = require("express");
+ var cors = require("cors");
+ var path = require("path");
+ 
+ var PORTA = process.env.PORT;
+ var AMBIENTE = process.env.AMBIENTE_PROCESSO;
+ 
+ // ------------------SUB-REQUISIÇÔES-----------------------------------
+ 
+ var app = express();
+ var index = require("./src/routes/index");
+
+ // ------------------MIDDLEWARER----------------------------------------
+ 
+ 
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+ 
+ // Pasta que guarda as views. Define o caminho principal, o inicio de toda rota começa por "Public"
+ app.use(express.static(path.join(__dirname, "public")));
+ 
+ app.use(cors());
+ 
+ app.use(express.static('public'))
+ 
+ // ------------------ROTAS-PADRÕES--------------------------------------
+ 
+ app.use("/", index);
+ 
+ // ---------------PORTA DO NAVEGADOR------------------------------------
+ 
+ app.listen(PORTA, function () {
+     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA}`);
+ });
